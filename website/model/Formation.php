80145fd9a26c211927c57c56c1a61afd87e179ab
Nicolas Sias <nicolas.sias.01@gmail.com>
2017-02-03 10:56:59 +0100

    Fixed connection DB
    Connection DB fixed but it's still awful

--- website/model/Formation.php
+++ website/model/Formation.php
@@ -14,14 +14,10 @@ class Formation {
     public function __construct($title)
     {
         $db_info = new ConnectionDB();
-        echo $db_info->server_name;
-        echo $db_info->db_name;
-        echo $db_info->user;
-        echo $db_info->password;
         //$sql = new PDO("mysql: host =localhost; dbname=1516he201220", "SIAS" , "Nicolas6B4g");
-        $sql = new PDO("mysql: host =".$db_info->server_name."; dbname=".$db_info->db_name.",".$db_info->user.",".$db_info->password."");
+        $sql = new PDO("mysql: host =".$db_info->server_name.";dbname=".$db_info->db_name."","".$db_info->user."","".$db_info->password."");
         $stmt = $sql->prepare("SELECT * FROM formation WHERE nomFormation = ?");
-        $stmt->execute($title);
+        $stmt->execute(array($title));
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
         if($result)
         {
@@ -32,7 +28,7 @@ class Formation {
         {
             $this->id = 0;
             $this->title = "Erreur SQL";
-            $this->content = "Erreur";
+            $this->content = "Erreur SQL du contenu";
         }
     }
 }
