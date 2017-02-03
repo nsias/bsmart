4bf39ac7f4802da6f0e498d40d29afe18cd84005
Nicolas Sias <nicolas.sias.01@gmail.com>
2017-02-03 11:21:08 +0100

    Display error
    Display error

--- website/index.php
+++ website/index.php
@@ -5,6 +5,9 @@
  * Date: 02-02-17
  * Time: 14:21
  */
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include_once("controller/Controller.php");
 
 $controller = new Controller();
