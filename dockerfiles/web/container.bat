a37efafe1738524fa8f53fab0eb1946102a4e4e2
simon <levraipp7@outlook.fr>
2017-02-06 22:53:30 +0100

    docker

---
+++ dockerfiles/web/container.bat
@@ -0,0 +1,8 @@
+sudo docker stop BsmartWeb1
+echo "stop ok"
+sudo docker rm BsmartWeb1
+sudo docker build -t web1 .
+
+sudo docker run -itd --name=BsmartWeb1 -v /home/user/site:/var/www/html -privileged web1
+
+
