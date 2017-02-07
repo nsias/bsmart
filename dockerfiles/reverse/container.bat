0fd9c8f1619281aa0136f8b24b978a6a95abc789
simon <levraipp7@outlook.fr>
2017-02-07 14:05:04 +0100

    docker

--- dockerfiles/reverse/container.bat
+++ dockerfiles/reverse/container.bat
@@ -2,6 +2,6 @@ sudo docker stop reverse
 echo "stop ok"
 sudo docker rm reverse
 sudo docker build -t reverse .
-sudo docker run -itd --name=reverse -p 80:80 -privileged reverse
+sudo docker run -itd --name=reverse -p 80:80 reverse
  
 
--- dockerfiles/web/container.bat
+++ dockerfiles/web/container.bat
@@ -3,6 +3,6 @@ echo "stop ok"
 sudo docker rm BsmartWeb1
 sudo docker build -t web1 .
 
-sudo docker run -itd --name=BsmartWeb1 -v /home/user/site:/var/www/html -privileged web1
+sudo docker run -itd --name=BsmartWeb1 -v /home/user/site:/var/www/html web1
 
 
